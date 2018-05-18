46487d2a7c0b4547ad599d54ee63bc10b238aced
shin1kt <nomeshi74@gmail.com>
2018-05-18 19:56:17 +0900

    Vue.jsで一覧を表示

---
+++ resources/assets/js/components/MyPage.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="container">
+    <div class="row">
+      <ul class="list-group">
+        <li v-for="(item, key) in items" :key=key class="list-group-item">{{item.title}} <time>{{item.date}}</time></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      items: null,
+    }
+  },
+  mounted: function() {
+    axios.get('/api/entry')
+    .then((res) => {
+      this.items = res.data.data;
+    });
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+time {
+  font-size:0.8em;
+  &::before {
+    content:"( ";
+  }
+  &::after {
+    content: " )";
+  }
+}
+</style>
