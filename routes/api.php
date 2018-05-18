cf78364fe181cfcc1656f593a2e84c3ce89456bc
shin1kt <nomeshi74@gmail.com>
2018-05-18 19:11:09 +0900

    一覧取得のエントリポイントを作成

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 
+use App\Entry;
+use App\Http\Resources\Entry as EntryResource;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +18,8 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/entry', function () {
+    return EntryResource::collection(Entry::all());
 });
